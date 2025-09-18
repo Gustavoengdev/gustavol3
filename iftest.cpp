013c31a2f3920e6bc6ed1749309ce5e80736ac9c
Gustavo Lima <gustavols2@hotmail.com>
2025-09-17 23:36:34 -0300

    Teste do Visual Studio no Linux
    Teste de funções if, print f e variáveis.

---
+++ iftest.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(){
+
+char age[3];
+char weight[3];
+char kg[3];
+
+printf("Digite sua altura\n");
+scanf("%s", &weight);
+
+printf("Digite sua idade\n");
+scanf("%s", &age);
+
+printf("Digite seu peso\n");
+scanf("%s", &kg);
+
+if (kg == "%s",68){
+
+
+printf("Peso %s kilos \n ", kg);
+printf("Idade %s anos \n", age);
+printf("Altura %s m \n ", weight);
+
+}
+
+else{
+        printf("dados inadequados");}
+
+return 0;}
